0002044032cc61ff350e088cd2e4950f2a16fec5
Jesray Garciano <jesray.garciano@gmail.com>
2019-08-07 14:42:13 +0800

    View functionalities

--- script.js
+++ script.js
@@ -1,73 +1,87 @@
 class Model {
     constructor() {
-      // The state of the model, an array of todo objects, prepopulated with some data
-      this.todos = [
-        { id: 1, text: 'Run a marathon', complete: false },
-        { id: 2, text: 'Plant a garden', complete: false },
-      ]
+        // The state of the model, an array of todo objects, prepopulated with some data
+        this.todos = [{
+                id: 1,
+                text: 'Run a marathon',
+                complete: false
+            },
+            {
+                id: 2,
+                text: 'Plant a garden',
+                complete: false
+            },
+        ]
     }
-  
+
     // Append a todo to the todos array
     addTodo(todo) {
-      this.todos = [...this.todos, todo]
+        this.todos = [...this.todos, todo]
     }
-  
+
     // Map through all todos, and replace the text of the todo with the specified id
     editTodo(id, updatedText) {
-      this.todos = this.todos.map(todo =>
-        todo.id === id ? { id: todo.id, text: updatedText, complete: todo.complete } : todo
-      )
+        this.todos = this.todos.map(todo =>
+            todo.id === id ? {
+                id: todo.id,
+                text: updatedText,
+                complete: todo.complete
+            } : todo
+        )
     }
-  
+
     // Filter a todo out of the array by id
     deleteTodo(id) {
-      this.todos = this.todos.filter(todo => todo.id !== id)
+        this.todos = this.todos.filter(todo => todo.id !== id)
     }
-  
+
     // Flip the complete boolean on the specified todo
     toggleTodo(id) {
-      this.todos = this.todos.map(todo =>
-        todo.id === id ? { id: todo.id, text: todo.text, complete: !todo.complete } : todo
-      )
+        this.todos = this.todos.map(todo =>
+            todo.id === id ? {
+                id: todo.id,
+                text: todo.text,
+                complete: !todo.complete
+            } : todo
+        )
     }
-  }
+}
 
 class View {
-  constructor() {
 
-    // The roote element
-    this.app = this.getElement('#root')
+    constructor() {
+        // The root element
+        this.app = this.getElement('#root')
 
-    // The title of the app
-    this.title = this.createElement('h1');
-    this.title.textContent = 'Todos';
+        // The title of the app
+        this.title = this.createElement('h1')
+        this.title.textContent = 'Todos'
 
-    // The form, with a [type="text"] input, and a submit button
-    this.form = this.createElement('form');
+        // The form, with a [type="text"] input, and a submit button
+        this.form = this.createElement('form')
 
-    this.input = this.createElement('input');
-    this.input.type = 'text'
-    this.input.placeholder = 'Add todo'
-    this.input.name = 'todo'
+        this.input = this.createElement('input')
+        this.input.type = 'text'
+        this.input.placeholder = 'Add todo'
+        this.input.name = 'todo'
 
-    this.submitButton = this.createElement('button')
-    this.submitButton.textContent = 'Submit'
+        this.submitButton = this.createElement('button')
+        this.submitButton.textContent = 'Submit'
 
-    // The visual representation of the todo list
-    this.todoList = this.createElement('ul', 'todo-list')
+        // The visual representation of the todo list
+        this.todoList = this.createElement('ul', 'todo-list')
 
-    // Append the input and submit button to the form
-    this.form.append(this.input, this.submitButton)
+        // Append the input and submit button to the form
+        this.form.append(this.input, this.submitButton)
 
-    // Append the title, form, and todo list to the app
-    this.app.append(this.title, this.form, this.todoList)
-  }
+        // Append the title, form, and todo list to the app
+        this.app.append(this.title, this.form, this.todoList)
+    }
 
-    // Create an element with an Optional CSS class
-
+    // Create an element with an optional CSS class
     createElement(tag, className) {
         const element = document.createElement(tag)
-        if (className) element.clasList.add(className)
+        if (className) element.classList.add(className)
 
         return element
     }
@@ -78,13 +92,70 @@ class View {
 
         return element
     }
+
+    get _todoText() {
+        return this.input.value
+    }
+
+    _resetInput() {
+        this.input.value = ''
+    }
+
+    displayTodos(todos) {
+        // Delete all nodes
+        while (this.todoList.firstChild) {
+            this.todoList.removeChild(this.todoList.firstChild)
+        }
+
+        // Show default message
+        if (todos.length === 0) {
+            const p = this.createElement('p')
+            p.textContent = 'Nothing to do! Add a task?'
+            this.todoList.append(p)
+        } else {
+
+            // Create todo item nodes for each todo in state
+            todos.forEach(todo => {
+                const li = this.createElement('li')
+                li.id = todo.id
+
+                // Each todo item will have a checkbox you can toggle
+                const checkbox = this.createElement('input')
+                checkbox.type = 'checkbox'
+                checkbox.checked = todo.complete
+
+                // The todo item text will be in a contenteditable span
+                const span = this.createElement('span')
+                span.contentEditable = true
+                span.classList.add('editable')
+
+                // If the todo is complete, it will have a strikethrough
+                if (todo.complete) {
+                    const strike = this.createElement('s')
+                    strike.textContent = todo.text
+                    span.append(strike)
+                } else {
+                    // Otherwise just display the text
+                    span.textContent = todo.text
+                }
+
+                // The todos will also have a delete button
+                const deleteButton = this.createElement('button', 'delete')
+                deleteButton.textContent = 'Delete'
+                li.append(checkbox, span, deleteButton)
+
+                // Append nodes to the todo list
+                this.todoList.append(li)
+            })
+        }
+    }
 }
 
 class Controller {
-  constructor(model, view) {
-    this.model = model
-    this.view = view
-  }
+    constructor(model, view) {
+        this.model = model
+        this.view = view
+    }
 }
 
 const app = new Controller(new Model(), new View())
